d04119cad0c120f458c8fc73ba5ca4766a4dbfa6
jesiqueira <edmar.ade@hotmail.com>
2020-12-11 18:44:45 +0000

    Adiciona metodos de pre e pos request e primeira rota
    faz a conexao com banco e a fecha após a requisição e gera a rota.

--- flaskr.py
+++ flaskr.py
@@ -14,3 +14,19 @@ app = Flask(__name__)
 def conectar_bd():
     return sqlite3.connect(DATABASE)
 
+
+@app.before_request
+def pre_requesicao():
+    g.bd = conectar_bd()
+
+@app.teardown_request
+def pos_requisicao():
+    g.bd.close()
+
+
+@app.route('/')
+def exibir_entradas():
+    sql = "SELECT titulo, texto FROM entradas ORDER BY id DESC"
+    cursor = g.bd.execute(sql)
+    entradas = [dict(titulo=titulo, texto=texto) for titulo, texto in cursor.fetchall()]
+    return render_template('exibir_entradas.html')
